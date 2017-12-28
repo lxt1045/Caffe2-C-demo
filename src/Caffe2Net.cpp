70f7f896492fcc869b364bad799b0b35e5b8ee25
vision <lxt1045@163.com>
2017-12-28 15:14:06 +0800

    修改CPU可用；增加笔者的训练结果，可直接编译可执行文件后使用；makefile有个依赖opencv的lib链接，已改；

--- src/Caffe2Net.cpp
+++ src/Caffe2Net.cpp
@@ -1,14 +1,51 @@
+#ifdef WITH_CUDA
 #include <caffe2/core/context_gpu.h>
-#include "Caffe2Net.h"
+#endif
 
-Caffe2Net::Caffe2Net(string initNet,string predictNet)
-:workspace(nullptr)
+#include "Caffe2Net.h"
+#include <cstring>
+/*
+Caffe2Net::Caffe2Net(string initNet, string predictNet)
+	: workspace(nullptr)
 {
 #ifdef WITH_CUDA
 	DeviceOption option;
 	option.set_device_type(CUDA);
 	new CUDAContext(option);
 #endif
+	//载入部署模型
+	NetDef init_net_def, predict_net_def;
+	CAFFE_ENFORCE(ReadProtoFromFile(initNet, &init_net_def));
+	CAFFE_ENFORCE(ReadProtoFromFile(predictNet, &predict_net_def));
+#ifdef WITH_CUDA
+	init_net_def.mutable_device_option()->set_device_type(CUDA);
+	predict_net_def.mutable_device_option()->set_device_type(CUDA);
+#else
+	init_net_def.mutable_device_option()->set_device_type(CPU);
+	predict_net_def.mutable_device_option()->set_device_type(CPU);
+#endif
+	//网络初始化
+	workspace.RunNetOnce(init_net_def);
+	//创建判别器
+	predict_net = CreateNet(predict_net_def, &workspace);
+}
+//*/
+Caffe2Net::Caffe2Net(string initNet, string predictNet)
+	: workspace(nullptr)
+{
+#ifdef WITH_CUDA
+	DeviceOption option;
+	option.set_device_type(CUDA);
+	new CUDAContext(option);
+#endif
+
+	//在这里增加自己的代码，以便支持 .pb 模型参数
+	// pbtxt OR .pb
+	std::string fileSuffix(".pb");
+	if (initNet.compare(initNet.length() - fileSuffix.length(), fileSuffix.length(), fileSuffix) == 0)
+	{
+	}
+
 	//载入部署模型
 	NetDef init_net_def, predict_net_def;
 	CAFFE_ENFORCE(ReadProtoFromFile(initNet, &init_net_def));
